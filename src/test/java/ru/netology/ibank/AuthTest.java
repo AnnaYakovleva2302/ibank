566ca5331be55c039474321d47f9b694b72779c9
Anna Yakovleva <_bel4onok_@mail.ru>
2021-12-06 23:46:27 +0300

    Write auth form tests

---
+++ src/test/java/ru/netology/ibank/AuthTest.java
@@ -0,0 +1,53 @@
+package ru.netology.ibank;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+class AuthTest {
+    private UserData user = new UserData();
+    private FormObject form = new FormObject();
+
+    @BeforeEach
+    void openBrowser() {
+        open("http://localhost:9999");
+    }
+
+    @Test
+    void ifUserExistsAndPasswordCorrect() {
+        user = AuthService.activateUser(user);
+        form.fillAndSubmit(user.getLogin(), user.getPassword());
+        form.getSuccessContainer().shouldHave(text("Личный кабинет"));
+    }
+
+    @Test
+    void ifUserExistsButPasswordIncorrect() {
+        user = AuthService.activateUser(user);
+        form.fillAndSubmit(user.getLogin(), user.getPassword() + "!");
+        form.getErrorContainer().shouldHave(text("Неверно указан логин или пароль"));
+    }
+
+    @Test
+    void ifUserDoesNotExist() {
+        user = AuthService.activateUser(user);
+        form.fillAndSubmit(user.getLogin() + "!", user.getPassword());
+        form.getErrorContainer().shouldHave(text("Неверно указан логин или пароль"));
+    }
+
+    @Test
+    void ifUserExistsAndPasswordCorrectButBlocked() {
+        user = AuthService.blockUser(user);
+        form.fillAndSubmit(user.getLogin(), user.getPassword());
+        form.getErrorContainer().shouldHave(text("Пользователь заблокирован"));
+    }
+
+    @Test
+    void ifUserExistsButPasswordIncorrectAndBlocked() {
+        user = AuthService.blockUser(user);
+        form.fillAndSubmit(user.getLogin(), user.getPassword() + "!");
+        form.getErrorContainer().shouldHave(text("Неверно указан логин или пароль"));
+    }
+}
